81448abb8d3d13750fce4ee77a82ce58a00e0a50
DamianPrg <balandowskie@gmail.com>
2015-08-12 22:37:46 +0100

    settings model & migration

---
+++ database/migrations/2015_08_12_213437_create_settings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('skin')->default('default');
+            $table->string('site_title')->default('LiteForo');
+            $table->boolean('registration_enabled')->default(true);
+            $table->boolean('login_enabled')->default(true);
+            $table->boolean('site_enabled')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('settings');
+    }
+}
