9c2fcb37237c88c8d652a30e477a3159973b961b
DamianPrg <balandowskie@gmail.com>
2015-08-15 12:56:50 +0100

    layout tweaking
    css form classes, applied new form to login, and lot work in css file.

--- resources/views/skins/default/auth/login.blade.php
+++ resources/views/skins/default/auth/login.blade.php
@@ -4,6 +4,7 @@
 
     <form action="{{ route('auth.do.login') }}" method="POST">
 
+<!--
         <div>
         <label for="username">Username</label>
         <input type="text" name="username">
@@ -18,6 +19,43 @@
 
         <div>
             <button type="submit">Login</button>
+        </div>
+        -->
+
+<div class='box'>
+        <div class='form'>
+        	<div class='form-element'>
+        		<div class='detail'>
+        			<label class='normal-text' for="username">Username</label>
+        			<br>
+        			<span class='muted-text small-text'>Your username</span>
+        		</div>
+        		<div class='input'>
+        		 	 <input type="text" name="username">
+        		</div>
+        	</div>
+        	<div class='form-element'>
+        		<div class='detail'>
+        			<label class='normal-text' for="password">Password</label>
+        			<br>
+        			<span class='muted-text small-text'>Your password</span>
+        		</div>
+        		<div class='input'>
+        		 	 <input type="password" name="password">
+        		</div>
+        	</div>
+        	        	<div class='form-element'>
+        		<div class='detail'>
+        			
+        		</div>
+        		<div class='input'>
+        		 	<button type="submit">Login</button>
+        		</div>
+        	</div>
+
+        	
+        </div>
+
         </div>
 
     </form>
--- resources/views/skins/default/layouts/default.blade.php
+++ resources/views/skins/default/layouts/default.blade.php
@@ -1,32 +1,86 @@
 <!DOCTYPE>
 <html>
 	<head>
-		<title></title>
+		<title>@yield('head.title', '')</title>
 
-		<link type="text/css" rel="stylesheet" href="{{URL::asset('css/default/styles.css')}}"
+		<!-- css -->
+				<link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css">
+
+		<link type="text/css" rel="stylesheet" href="{{URL::asset('css/default/styles.css')}}">
+
+
+		<!-- css -->
 	</head>
 
 	<body>
-		<div class='wrapper'>
+	    <div class='wrapper'>
 
-			<div class='container'>
+				<header class='header'>
+					LiteForo
+				</header>
 
-				<nav class='nav'>
-					<a href='{{ url('/') }}'>Forums</a>
-					
-					@yield('nav_links', '')
+		
+
+	    </div>
+		<!-- wrapper -->
+		<div class='wrapper wrapper-styled'>
+
+
+							<nav class='nav nice-link-color'>
+					<a href='#'>Forums</a>
+
 				</nav>
+		
 
-				<section class='content'>
-					@yield('content', '')
-
-					{{ $content or '' }}
-				</section>
-
-				<footer>Powered by LiteForo</footer>
-
+			<div class='links'>
+				@yield('links')
 			</div>
 
+			<div class='content'>
+				<div class='main'>
+					@yield('content')
+				</div>
+
+				<aside class='side'>
+								<div class='side-element'>
+						<div class='side-element-header'>
+						Side
+						</div>
+						This is side element.
+					</div>
+				</aside>
+			</div>
+
+			<div class='links'>
+				@yield('links')
+			</div>
+
+
+		
+
+			
+
+		</div>
+		<!-- wrapper -->	
+		<div class='wrapper wrapper-footer wrapper-ns-without-border'>
+		<footer class='footer'>
+			<div class='left'>
+				<div>Quick links</div>
+				<nav class='footer-nav'>
+					<div><a href='#'>Forums</a></div>
+					<div><a href='#'>Staff</a></div>
+					<div><a href='#'>Contact</a></div>
+				</nav>
+			</div>
+
+			<div class='mid'>
+				<!-- middle -->
+			</div>
+
+			<div class='right'>
+			Powered by LiteForo
+			</div>
+		</footer>
 		</div>
 	</body>
 </html>
